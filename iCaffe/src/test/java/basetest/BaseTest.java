4df56629ee0c1417eb9d32d998352e242d3ca6ef
Akashvarun12 <akash.varun@hanssupport.com>
2025-09-03 17:10:49 +0530

    commit seven add testcase icaffe

--- iCaffe/src/test/java/basetest/BaseTest.java
+++ iCaffe/src/test/java/basetest/BaseTest.java
@@ -18,11 +18,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.markuputils.ExtentColor;
-import com.aventstack.extentreports.markuputils.MarkupHelper;
+
 
 import webutil.ExcelDataSuplier;
 import webutil.WebUtil;
@@ -31,18 +27,18 @@ public class BaseTest {
 	protected WebUtil utilObj ;
 
 	
-	@DataProvider
-	public Object[][] provideLoginData() {
-
-		 List<Map<String,String>> testData = ExcelDataSuplier.setExcelFile("TestSheet");
-		 int dataMapCount=testData.size();
-		 Object[][] allData= new Object[dataMapCount][1];
-		 for(int i=0;i<=dataMapCount-1;i++) {
-			allData[i][0] = testData.get(i);
-		 }
-		 
-	return allData;    
-	}
+//	@DataProvider
+//	public Object[][] provideLoginData() {
+//
+//		 List<Map<String,String>> testData = ExcelDataSuplier.setExcelFile("TestSheet");
+//		 int dataMapCount=testData.size();
+//		 Object[][] allData= new Object[dataMapCount][1];
+//		 for(int i=0;i<=dataMapCount-1;i++) {
+//			allData[i][0] = testData.get(i);
+//		 }
+//		 
+//	return allData;    
+//	}
 
 
 	@BeforeSuite
@@ -50,15 +46,15 @@ public class BaseTest {
 		WebUtil.genrateExtentReport();
 	}
 
-	@BeforeTest
-	public void importFileFromExcel() {
-
-	}
-
-	@BeforeClass
-	public void beforeClass() {
-//		utilObj.genrateExtentReport();
-	}
+//	@BeforeTest
+//	public void importFileFromExcel() {
+//
+//	}
+//
+//	@BeforeClass
+//	public void beforeClass() {
+////		utilObj.genrateExtentReport();
+//	}
 
 	@Parameters({"browser","URL", "userName", "password"})
 	@BeforeMethod
---
+++ iCaffe/src/test/java/icaffetestcases/ICaffeMastersTestcase.java
@@ -0,0 +1,27 @@
+package icaffetestcases;
+
+import org.testng.annotations.Test;
+
+import basetest.BaseTest;
+
+public class ICaffeMastersTestcase extends BaseTest {
+
+	
+	@Test
+	public void verifyMasterPartyExporter() {
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
